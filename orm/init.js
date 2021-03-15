c9a533705fab8fe4aab9469e5897dc1f0a9a3bb5
Kim Ebert <kim@indicio.tech>
2021-03-15 15:45:08 -0600

    Fix db name
    Signed-off-by: Kim Ebert <kim@indicio.tech>

--- orm/init.js
+++ orm/init.js
@@ -2,7 +2,7 @@ const {Sequelize} = require('sequelize')
 
 exports.connect = function () {
   const sequelize = new Sequelize('development', 'development', 'development', {
-    host: 'lab-db',
+    host: 'db',
     dialect: 'postgres',
     logging: false, // console.log, // log to console or false (no logging of database queries)
     omitNull: true,
