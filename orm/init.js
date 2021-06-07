6c4aa6d8874657dd5cf52d170931291fb1249396
Kim Ebert <kim@indicio.tech>
2021-06-07 15:47:27 -0600

    feat: setup verifier
    Signed-off-by: Kim Ebert <kim@indicio.tech>

--- orm/init.js
+++ orm/init.js
@@ -1,7 +1,7 @@
 const {Sequelize} = require('sequelize')
 
 exports.connect = function () {
-  const sequelize = new Sequelize('development', 'development', 'development', {
+  const sequelize = new Sequelize('verifier', 'verifier', 'verifier', {
     host: 'verifier-db',
     dialect: 'postgres',
     logging: false, // console.log, // log to console or false (no logging of database queries)
